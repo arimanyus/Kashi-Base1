f519153ed814343e6b9f5653703da7737471f03f
Vivek Tiwari <35337366+arimanyus@users.noreply.github.com>
2020-09-20 21:08:32 +0530

    Module update #3

--- static/website/js/module.js
+++ static/website/js/module.js
@@ -3,3 +3,19 @@ function gpuCal() {
     var gpu = document.getElementById('gpu').value;
     alert(gpu);
     }
+
+var GPUs = { 
+    AMD: ['Athlon 660', 'GX340'],
+    Nvidia: ['GTX 1660', 'GTX 1660 Ti']   
+ }
+
+ function gpuOptions(value) {
+    if (value.length == 0) document.getElementById("gpu").innerHTML = "<option></option>";
+    else {
+        var gpuSelect = "";
+        for (categoryId in GPUs[value]) {
+            gpuSelect += "<option>" + GPUs[value][categoryId] + "</option>";
+        }
+        document.getElementById("GPUSel").innerHTML = gpuSelect;
+    }
+}
