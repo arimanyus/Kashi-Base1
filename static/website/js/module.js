809689d9c361ccdeb6ecbd4ea9ef9c238bfed139
Vivek Tiwari <35337366+arimanyus@users.noreply.github.com>
2020-09-28 14:22:09 +0530

    CPU Model

--- static/website/js/module.js
+++ static/website/js/module.js
@@ -3,7 +3,7 @@ function gpuCal() {
     alert(gpu);
     }
 
- function gpuOptions(value) {
+function gpuOptions(value) {
     if (value.length == 0) document.getElementById("gpu").innerHTML = "<option></option>";
     else {
         var gpuSelect = "";
@@ -11,5 +11,16 @@ function gpuCal() {
             gpuSelect += "<option>" + GPUs[value][categoryId][2] + "</option>";
         }
         document.getElementById("GPUSel").innerHTML = gpuSelect;
+    }
+}
+
+function cpuOptions(value) {
+    if (value.length == 0) document.getElementById("cpu").innerHTML = "<option></option>";
+    else {
+        var cpuSelect = "";
+        for (categoryId in CPUs[value]) {
+            cpuSelect += "<option>" + CPUs[value][categoryId][2] + "</option>";
+        }
+        document.getElementById("CPUSel").innerHTML = cpuSelect;
     }
 }
